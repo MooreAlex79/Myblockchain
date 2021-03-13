4f2fadfc84fedee2925d6a53c0733b5ed4564df9
Alex Moore <78249353+MooreAlex79@users.noreply.github.com>
2021-03-12 21:35:19 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
-#include <iostream>
-#include "sha256.h"
- 
-using std::string;
-using std::cout;
-using std::endl;
- 
-int main(int argc, char *argv[])
-{
-    string input = "grape";
-    string output1 = sha256(input);
- 
-    cout << "sha256('"<< input << "'):" << output1 << endl;
+#include "Blockchain.h"
+
+int main() {
+    Blockchain bChain = Blockchain();
+
+    cout << "Mining block 1..." << endl;
+    bChain.AddBlock(Block(1, "Block 1 Data"));
+
+    cout << "Mining block 2..." << endl;
+    bChain.AddBlock(Block(2, "Block 2 Data"));
+
+    cout << "Mining block 3..." << endl;
+    bChain.AddBlock(Block(3, "Block 3 Data"));
+
     return 0;
 }
+
+gcc -lstdc++ \
+    -o TestChain \
+    -std=c++11 \
+    -stdlib=libc++ \
+    -x c++ \
+    main.cpp Block.cpp Blockchain.cpp sha256.cpp
+./SmartChain
